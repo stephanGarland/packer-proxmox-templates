d279dae8ca36d28a0a4b351b285cdee82626113c
Stephan Garland <stephan.garland@logicmonitor.com>
2021-01-07 14:21:55 -0600

    bugfix

--- build_menu.sh
+++ build_menu.sh
@@ -34,16 +34,16 @@ call_dialog() {
             --backtitle "Proxmox Template Creation" \
             --title "Create A New Template" \
             --form "Template Options" 15 50 0 \
-                "Node: "     1 1 "$vm_node"      1 10 10 0 \ 
-                "Role: "     1 1 "$vm_role"      2 10 10 0 \
-                "CPU Type: " 2 1 "$vm_cpu_type"  3 10 10 0 \
-                "Sockets: "  3 1 "$vm_sockets"   4 10 10 0 \
-                "Cores: "    4 1 "$vm_cores"     5 10 10 0 \
-                "Memory: "   5 1 "$vm_mem"       6 10 10 0 \
-                "Disk: "     6 1 "$vm_disk"      7 10 10 0 \
-                "ZFS: "      7 1 "$vm_zfs"       8 10 10 0 \
-                "ZFS: "      8 1 "$vm_zsh"       9 10 10 0 \
-                "VM ID: "    9 1 "$vm_id"        10 10 10 0 \
+                "Node: "     1 1 "$vm_node"      1 10 10 0 \
+                "Role: "     2 1 "$vm_role"      2 10 10 0 \
+                "CPU Type: " 3 1 "$vm_cpu_type"  3 10 10 0 \
+                "Sockets: "  4 1 "$vm_sockets"   4 10 10 0 \
+                "Cores: "    5 1 "$vm_cores"     5 10 10 0 \
+                "Memory: "   6 1 "$vm_mem"       6 10 10 0 \
+                "Disk: "     7 1 "$vm_disk"      7 10 10 0 \
+                "ZFS: "      8 1 "$vm_zfs"       8 10 10 0 \
+                "ZFS: "      9 1 "$vm_zsh"       9 10 10 0 \
+                "VM ID: "    10 1 "$vm_id"       10 10 10 0 \
             --and-widget --insecure \
             --title "Create A New Template" \
             --passwordform "Template Passwords" 15 50 0 \
